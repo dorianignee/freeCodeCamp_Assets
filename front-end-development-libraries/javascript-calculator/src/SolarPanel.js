0860a18f7250fe9503a8428ff41a36d65a150e1b
Dorian Ignee <dorian.ignee@posterselect.de>
2023-10-06 14:41:15 +0200

    added a link to my github account

--- front-end-development-libraries/javascript-calculator/src/SolarPanel.js
+++ front-end-development-libraries/javascript-calculator/src/SolarPanel.js
@@ -3,8 +3,8 @@ import styles from './App.module.css';
 export default function SolarPanel() {
     return (
         <div className={styles.solarRow}>
-            <span className={styles.brandMain}>Dorian Instruments</span>
-            <span className={styles.brandSub}>DI 30S</span>
+            <span className={styles.brandMain}><a href="https://github.com/dorianignee/freeCodeCamp_Challenges/">Dorian Instruments</a></span>
+            <span className={styles.brandSub}><a href="https://github.com/dorianignee/freeCodeCamp_Challenges/">DI 30S</a></span>
             <div className={styles.solarPanel}/>
         </div>
     )
