5492148438445d557a9399bd6e29d023658c0257
Dorian Ignee <dorian.ignee@posterselect.de>
2024-03-27 11:09:14 +0100

    fix: concurrent execution

--- back-end-development-and-APIs/boilerplate-project-urlshortener/index.js
+++ back-end-development-and-APIs/boilerplate-project-urlshortener/index.js
@@ -9,7 +9,7 @@ const dns = require('dns');
 // Basic Configuration
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
-mongoose.connect(process.env.MONGO_URI /*, clientOptions */);
+mongoose.connect(process.env.MONGO_URI, clientOptions);
 
 const port = process.env.PORT || 3000;
 
@@ -33,24 +33,23 @@ const Url = mongoose.model('Url', urlSchema);
 
 // Redirect to long url
 app.get('/api/shorturl/:shorturl', function(req, res) {
-
   const urlId = Number(req.params.shorturl);
 
   if (isNaN(urlId)) {
-
     res.json({"error": "invalid url"});
     return;
   }
 
   Url.findOne({shortUrl: urlId})
     .then(url => {
-
-      if (/^https?:\/\//.test(url.url)) {
-
-        res.redirect(url.url);
+      if (url) {
+        if (/^https?:\/\//.test(url.url)) {
+          res.redirect(url.url);
+        } else {
+          res.redirect("http://" + url.url)
+        }
       } else {
-
-        res.redirect("http://" + url.url)
+        res.send("Unknown short url.");
       }
     })
     .catch(err => console.log(err));
@@ -62,35 +61,35 @@ app.post('/api/shorturl', (req, res) => {
   
   // check if url is valid
   const hostname = /^(?:.*?:\/\/)?(.*?)(?:\/|$|\?)/.exec(longUrl)[1]
-  dns.lookup(hostname, (err) => {
+  dns.lookup(hostname, (err, ip) => {
     if (err) {
       res.json({"error": "invalid url"});
       return;
     }
-  })
 
-  // check if url is already in database
-  Url.findOne({url: longUrl})
+    // check if url is already in database
+    Url.findOne({url: longUrl})
     .then(record => {
       if (record) {
         res.json({ "original_url": record.url, "short_url": record.shortUrl });
         return;
       }
-    });
 
-  // insert new record
-  Url.countDocuments()
-    .then(docCount => {
-      const entry = new Url({ url: longUrl, shortUrl: docCount });
-      entry
-        .save()
-        .then(record => {
-          res.json({ "original_url": record.url, "short_url": record.shortUrl });
-          return;
-        })
-        .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
+      // insert new record
+      Url.countDocuments()
+      .then(docCount => {
+        const entry = new Url({ url: longUrl, shortUrl: docCount });
+        entry
+          .save()
+          .then(record => {
+            res.json({ "original_url": record.url, "short_url": record.shortUrl });
+            return;
+          })
+          .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));
+    });
+  })
 })
 
 app.listen(port, function() {
