c3a301a8870d99d4d388e8c46b8f3b839a7fa5c1
Dorian Ignee <dorian.ignee@posterselect.de>
2024-04-02 11:24:23 +0200

    feat: completed api.js and convertHandler.js

--- quality-assurance/boilerplate-project-metricimpconverter/controllers/convertHandler.js
+++ quality-assurance/boilerplate-project-metricimpconverter/controllers/convertHandler.js
@@ -1,42 +1,86 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
-    
-    return result;
+    let numString = /^[\d\.\/]*/.exec(input)[0];
+    if (numString === '') return 1; // if no number is provided, the function should convert 1 unit
+
+    let {numerator, denominator} = /^(?<numerator>\d+(\.\d+)?)(\/(?<denominator>\d+(\.\d+)?))?$/.exec(numString).groups;
+    return denominator === undefined ? numerator: numerator / denominator;
   };
   
   this.getUnit = function(input) {
-    let result;
+    let result = /^[\d\.\/]*(gal|l|mi|km|lbs|kg)$/.exec(input.toLowerCase())[1];
     
-    return result;
+    return result === 'l'?'L':result;
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+    const unitTranslation = {
+      "gal": "L",
+      "L": "gal",
+      "mi": "km",
+      "km": "mi",
+      "lbs": "kg",
+      "kg": "lbs"
+    }
+
+    return unitTranslation[initUnit];
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+    const spellOut = {
+      "gal": "gallons",
+      "L": "liters",
+      "mi": "miles",
+      "km": "kilometers",
+      "lbs": "pounds",
+      "kg": "kilograms"
+    }
+
+    return spellOut[initUnit];
   };
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
+
+    const calculation = {
+      "gal": {
+        factor: galToL,
+        multiply: true
+      },
+      "L": {
+        factor: galToL,
+        multiply: false
+      },
+      "lbs": {
+        factor: lbsToKg,
+        multiply: true
+      },
+      "kg": {
+        factor: lbsToKg,
+        multiply: false
+      },
+      "mi": {
+        factor: miToKm,
+        multiply: true
+      },
+      "km": {
+        factor: miToKm,
+        multiply: false
+      },
+    }
+
+    const convert = calculation[initUnit];
     
-    return result;
+    return convert.multiply
+         ? initNum * convert.factor
+         : initNum / convert.factor;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+    return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
   };
   
 }
--- quality-assurance/boilerplate-project-metricimpconverter/routes/api.js
+++ quality-assurance/boilerplate-project-metricimpconverter/routes/api.js
@@ -7,4 +7,38 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.get('api/convert', (req, res) => {
+    if (!req.query.contains("input")) return res.status(400).send("Missing required field 'input'");
+
+    // try to read number and unit
+    let initNum;
+    let initUnit;
+    try {
+      initNum = convertHandler.getNum(req.query.input)
+    } catch (e) {
+      initNum = NaN;
+    }
+
+    try {
+      initUnit = convertHandler.getUnit(req.query.input)
+    } catch (e) {
+      if (isNaN(initNum)) return res.status(400).send("Invalid number and unit");
+      else return res.status(400).send("Invalid unit");
+    }
+
+    if (isNaN(initNum)) return res.status(400).send("Invalid number");
+
+    // convert and send result
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string
+    });
+  })
+
 };
